ce984f593f5c111106904dfd8fc28675f62d4983
Aravindhan-dev-acc <aravindhan.nandu.1999@gmail.com>
2024-08-07 14:04:09 +0530

    Update MEM_TO_MAT.py
    comments removed

--- MEM_TO_MAT.py
+++ MEM_TO_MAT.py
@@ -1,5 +1,3 @@
-
-
 #Startup Code Begin
 import param
 import model
@@ -7,13 +5,6 @@ import Transform3D
 #Startup Code End
 
 def mem_to_mtrl(m1,m2):
-    '''
-    :type m1: model member object
-    :param m1: the member the user is making into a material
-    :type m2: model member object
-    :param m2: the member object the new material is being added to
-    '''
-
     mt = m1.Material[0]
     if mt.main_mtrl==True:
         pass
@@ -28,7 +19,6 @@ def mem_to_mtrl(m1,m2):
     model.EraseMember(m1.number)
 
     return
-
 
 def main():
     if model.LocateMultiple('Select Members to Make Material',model.IsMember):
@@ -52,7 +42,6 @@ def main():
             mem_to_mtrl(m,m2)
 
     return
-
 
 if __name__=='__main__':
     main()
